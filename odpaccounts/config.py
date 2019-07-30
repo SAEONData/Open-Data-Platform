9f7a8d0c0bab5b401cb199c1bdca79a7e9cab5eb
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2019-07-30 17:43:06 +0200

    Minor config improvements
    Including option for emitting SQLAlchemy database calls to stderr.

--- odpaccounts/config.py
+++ odpaccounts/config.py
@@ -2,12 +2,14 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ['FLASK_SECRET_KEY']
-    SERVER_NAME = os.environ['FLASK_SERVER_NAME']
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
+    SERVER_NAME = os.getenv('FLASK_SERVER_NAME')
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_ECHO = os.getenv('DATABASE_ECHO', '').lower() == 'true'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    HYDRA_ADMIN_URL = os.environ['HYDRA_ADMIN_URL']
-    HYDRA_PUBLIC_URL = os.environ['HYDRA_PUBLIC_URL']
-    HYDRA_CLIENT_ID = os.environ['HYDRA_CLIENT_ID']
-    HYDRA_CLIENT_SECRET = os.environ['HYDRA_CLIENT_SECRET']
+    HYDRA_ADMIN_URL = os.getenv('HYDRA_ADMIN_URL')
+    HYDRA_PUBLIC_URL = os.getenv('HYDRA_PUBLIC_URL')
+    HYDRA_CLIENT_ID = os.getenv('HYDRA_CLIENT_ID')
+    HYDRA_CLIENT_SECRET = os.getenv('HYDRA_CLIENT_SECRET')
