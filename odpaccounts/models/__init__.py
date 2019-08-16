7826870320107faa400d4b0df95ffee8ea30c3fa
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2019-08-16 13:58:23 +0200

    Include all tables for startup initialization
    This ensures that the 'initdb' cli command creates all tables, even the ones not yet in use.

--- odpaccounts/models/__init__.py
+++ odpaccounts/models/__init__.py
@@ -4,4 +4,13 @@ db = SQLAlchemy()
 
 
 def init_app(app):
+    from .hydra_token import HydraToken
+    from .user import User
+    from .role import Role
+    from .scope import Scope
+    from .scoped_role import scoped_role
+    from .assigned_role import assigned_role
+    from .institution import Institution
+    from .institution_registry import InstitutionRegistry
+    from .institutional_user import institutional_user
     db.init_app(app)
