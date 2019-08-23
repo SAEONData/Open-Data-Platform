3830a2c04e006999a4fc70f52f46988573cba99f
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2019-08-23 17:26:55 +0200

    Add home and logout links to admin UI nav

--- odpidentity/views/__init__.py
+++ odpidentity/views/__init__.py
@@ -7,21 +7,27 @@ login_manager = LoginManager()
 login_manager.login_view = 'hydra.login'
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
 def init_app(app):
+    login_manager.init_app(app)
+
     from . import home, user, hydra_client, hydra_auth, admin
     app.register_blueprint(home.bp, url_prefix='/')
     app.register_blueprint(user.bp, url_prefix='/user')
     app.register_blueprint(hydra_client.bp, url_prefix='/user')
     app.register_blueprint(hydra_auth.bp, url_prefix='/auth')
-    login_manager.init_app(app)
-    admin_views = Admin(app, name='ODP Administration', index_view=admin.home)
+
+    admin_views = Admin(app,
+                        name='ODP Admin',
+                        index_view=admin.home,
+                        base_template='admin_base.html',
+                        )
     admin_views.add_view(admin.users)
     admin_views.add_view(admin.roles)
     admin_views.add_view(admin.scopes)
     admin_views.add_view(admin.institutions)
     admin_views.add_view(admin.institution_registries)
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
