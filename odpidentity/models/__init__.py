608d68446f915908b1180c2a3842cb0c8bce0dc3
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2019-08-29 10:41:55 +0200

    Rename table

--- odpidentity/models/__init__.py
+++ odpidentity/models/__init__.py
@@ -7,9 +7,9 @@ def init_app(app):
     from .hydra_token import HydraToken
     from .user import User
     from .role import Role
+    from .user_role import user_role
     from .scope import Scope
     from .scoped_role import scoped_role
-    from .assigned_role import assigned_role
     from .institution import Institution
     from .institution_registry import InstitutionRegistry
     from .institutional_user import institutional_user
--- odpidentity/models/user_role.py
+++ odpidentity/models/user_role.py
@@ -1,10 +1,10 @@
 from . import db
 
-# association table for user-institution-role-scope relationship;
-# for a given user affiliated to a given institution, this table indicates the role(s) assigned to that
-# user within the context of that institution, and the scope(s) to which the role(s) apply
-assigned_role = db.Table(
-    'assigned_role',
+# association table for the user-institution-role-scope relationship;
+# for a given user affiliated to a given institution, this table indicates the role(s) assigned to
+# that user within the context of that institution, and the scope(s) to which the role(s) apply
+user_role = db.Table(
+    'user_role',
     db.Column('user_id', db.String, primary_key=True),
     db.Column('institution_id', db.Integer, primary_key=True),
     db.Column('role_id', db.Integer, primary_key=True),
@@ -13,13 +13,13 @@ assigned_role = db.Table(
     db.ForeignKeyConstraint(
         ['user_id', 'institution_id'],
         ['institutional_user.user_id', 'institutional_user.institution_id'],
-        name='assigned_role_user_institution_fkey',
+        name='user_role_fkey_user_institution',
         ondelete='CASCADE',
     ),
     db.ForeignKeyConstraint(
         ['role_id', 'scope_id'],
         ['scoped_role.role_id', 'scoped_role.scope_id'],
-        name='assigned_role_role_scope_fkey',
+        name='user_role_fkey_role_scope',
         ondelete='CASCADE',
     ),
 )
