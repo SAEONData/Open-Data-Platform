c2945987a5db8667eb1f6f8fb2da526bed372cf2
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-01-08 08:57:14 +0200

    Load models up front
    To enable SQLAlchemy to determine relationships.

--- odpidentity/models/__init__.py
+++ odpidentity/models/__init__.py
@@ -1,5 +1,14 @@
 def init_app(app):
     from odpaccounts.db import session
+    from odpaccounts.models.user import User
+    from odpaccounts.models.member import Member
+    from odpaccounts.models.privilege import Privilege
+    from odpaccounts.models.role import Role
+    from odpaccounts.models.scope import Scope
+    from odpaccounts.models.capability import Capability
+    from odpaccounts.models.institution import Institution
+    from odpaccounts.models.institution_registry import InstitutionRegistry
+    from .hydra_token import HydraToken
 
     # ensure that the db session is closed and disposed after each request
     @app.teardown_appcontext
