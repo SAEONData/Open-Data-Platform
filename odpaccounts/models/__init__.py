216a20b80cd26a4fb57ee71cdbf44f86bc8c8e19
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-01-10 16:05:28 +0200

    Remove table creation / deletion functions from this library
    These functions should rather be performed directly in the Admin service, which is used to initialize the database.

--- odpaccounts/models/__init__.py
+++ odpaccounts/models/__init__.py
@@ -1,13 +1,3 @@
 from sqlalchemy.ext.declarative import declarative_base
 
-from ..db import engine
-
 Base = declarative_base()
-
-
-def create_all():
-    Base.metadata.create_all(bind=engine)
-
-
-def drop_all():
-    Base.metadata.drop_all(bind=engine)
