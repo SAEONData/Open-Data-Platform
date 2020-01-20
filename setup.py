45f6497c3596dc5b139314df2d97d51d3b71ede4
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-01-20 20:25:56 +0200

    Standardise dependency specifications
    - minimal set of abstract requirements in setup.py, facilitates dev/test install
    - move requirements.txt with full environment freeze to deployment project

--- setup.py
+++ setup.py
@@ -12,8 +12,11 @@ setup(
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        # use requirements.txt
-    ],
     python_requires='~=3.6',
+    install_requires=[
+        'requests',
+    ],
+    extras_require={
+        'test': ['pytest', 'coverage']
+    },
 )
