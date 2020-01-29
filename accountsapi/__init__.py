99d542254e49d41c6d2a717f1268e8d51a47092b
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-01-29 11:28:37 +0200

    Use DB session via Fastapi dependency
    Allows us to properly set up and tear down the session before and after each request.

---
+++ accountsapi/__init__.py
@@ -0,0 +1,18 @@
+def db_session():
+    """
+    Database session dependency.
+    :return: SQLAlchemy session
+    """
+    from odpaccounts.db import session
+    from odpaccounts.models.user import User
+    from odpaccounts.models.member import Member
+    from odpaccounts.models.privilege import Privilege
+    from odpaccounts.models.role import Role
+    from odpaccounts.models.scope import Scope
+    from odpaccounts.models.capability import Capability
+    from odpaccounts.models.institution import Institution
+    from odpaccounts.models.institution_registry import InstitutionRegistry
+    try:
+        yield session
+    finally:
+        session.remove()
