c651d732ab068385b0c24f27151dbc76db4b9888
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-01-29 10:20:08 +0200

    Parameterize the session
    Rather than importing directly from the db module, so as to allow the calling application to manage the session lifecycle itself

--- odpaccounts/authorization/utils.py
+++ odpaccounts/authorization/utils.py
@@ -1,13 +1,14 @@
 from typing import List
 
-from ..db import session
+from sqlalchemy.orm import Session
+
 from ..models.privilege import Privilege
 from ..models.user import User
 from ..models.scope import Scope
 from ..authorization.models import AuthorizedUser, Capacity
 
 
-def create_authorized_user(user: User, scopes: List[str]) -> AuthorizedUser:
+def create_authorized_user(db_session: Session, user: User, scopes: List[str]) -> AuthorizedUser:
     """
     Create an :class:`AuthorizedUser` instance, which is suitable for attachment to the
     access token for this user. This data will be recoverable from the 'ext' attribute
@@ -17,6 +18,7 @@ def create_authorized_user(user: User, scopes: List[str]) -> AuthorizedUser:
     If the user is a superuser, privileges will be an empty list, since a superuser
     can do anything anyway.
 
+    :param db_session: SQLAlchemy session
     :param user: a User instance
     :param scopes: list of scopes being requested for the token
     :return: AuthorizedUser
@@ -28,7 +30,7 @@ def create_authorized_user(user: User, scopes: List[str]) -> AuthorizedUser:
             capacities=[],
         )
 
-    privileges = session.query(Privilege).filter_by(user_id=user.id) \
+    privileges = db_session.query(Privilege).filter_by(user_id=user.id) \
         .join(Scope, Privilege.scope_id == Scope.id).filter(Scope.key.in_(scopes)) \
         .all()
 
