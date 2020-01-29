bec7c42cb59a73b49dce0dfcede5842a33a3dfee
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-01-29 15:35:55 +0200

    Rename variables and aliases
    To more clearly distinguish between ORM and schema models.

--- accountsapi/routers/institution.py
+++ accountsapi/routers/institution.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import NoResultFound
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-from odpaccounts.models.institution import Institution as SQLInstitution
-from odpaccounts.models.institution_registry import InstitutionRegistry as SQLRegistry
+from odpaccounts.models.institution import Institution as InstitutionORM
+from odpaccounts.models.institution_registry import InstitutionRegistry as RegistryORM
 
 from .. import db_session
 from ..models.institution import Institution
@@ -18,18 +18,18 @@ async def create_or_update_institution(
         institution: Institution,
         session: Session = Depends(db_session),
 ):
-    sqlinstitution = session.query(SQLInstitution).filter_by(key=institution.key).one_or_none()
-    if not sqlinstitution:
-        sqlinstitution = SQLInstitution(key=institution.key)
+    institution_orm = session.query(InstitutionORM).filter_by(key=institution.key).one_or_none()
+    if not institution_orm:
+        institution_orm = InstitutionORM(key=institution.key)
 
-    sqlinstitution.name = institution.name
+    institution_orm.name = institution.name
     try:
-        sqlinstitution.parent = session.query(SQLInstitution).filter_by(key=institution.parent_key).one() if institution.parent_key else None
-        sqlinstitution.registry = session.query(SQLRegistry).filter_by(key=institution.registry_key).one()
+        institution_orm.parent = session.query(InstitutionORM).filter_by(key=institution.parent_key).one() if institution.parent_key else None
+        institution_orm.registry = session.query(RegistryORM).filter_by(key=institution.registry_key).one()
     except NoResultFound:
         raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, "Referenced entity not found.")
 
-    session.add(sqlinstitution)
+    session.add(institution_orm)
     session.commit()
 
     return institution
