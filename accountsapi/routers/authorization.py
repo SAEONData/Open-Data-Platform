7f4159ec588ce4f5f4784c5db76b7b2ff4c4cd35
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-01-29 15:28:10 +0200

    Rename models as per library

--- accountsapi/routers/authorization.py
+++ accountsapi/routers/authorization.py
@@ -3,14 +3,14 @@ from fastapi.exceptions import HTTPException
 from starlette.requests import Request
 
 from hydra import HydraAdminClient, HydraAdminError
-from odpaccounts.authorization.models import AccessToken, AuthorizedUser
+from odpaccounts.auth.models import AccessToken, AccessRights
 
 from ..models.authorization import TokenIn
 
 router = APIRouter()
 
 
-@router.post('/', response_model=AuthorizedUser)
+@router.post('/', response_model=AccessRights)
 async def authorize(
         token_in: TokenIn,
         request: Request,
@@ -26,9 +26,9 @@ async def authorize(
             token_in.require_scope,
             token_in.require_audience,
         ))
-        authorized_user = AuthorizedUser(**access_token.ext.dict())
-        assert authorized_user.user_id == access_token.sub
-        return authorized_user
+        access_rights = AccessRights(**access_token.ext.dict())
+        assert access_rights.user_id == access_token.sub
+        return access_rights
 
     except HydraAdminError as e:
         raise HTTPException(status_code=e.status_code, detail=e.error_detail)
