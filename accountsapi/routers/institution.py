be6fbe2f111075311e3c34d568dc0a4abb7203e7
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-01-29 11:30:42 +0200

    Implement institution create/update API

--- accountsapi/routers/institution.py
+++ accountsapi/routers/institution.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-from odpaccounts.db import session as db_session
 from odpaccounts.models.institution import Institution as SQLInstitution
 from odpaccounts.models.institution_registry import InstitutionRegistry as SQLRegistry
 
+from .. import db_session
 from ..models.institution import Institution
 
 router = APIRouter()
@@ -12,13 +16,20 @@ router = APIRouter()
 @router.post('/', response_model=Institution)
 async def create_or_update_institution(
         institution: Institution,
+        session: Session = Depends(db_session),
 ):
-    sqlinstitution = db_session.query(SQLInstitution).filter_by(key=institution.key).one_or_none()
+    sqlinstitution = session.query(SQLInstitution).filter_by(key=institution.key).one_or_none()
     if not sqlinstitution:
         sqlinstitution = SQLInstitution(key=institution.key)
+
     sqlinstitution.name = institution.name
-    sqlinstitution.parent = db_session.query(SQLInstitution).filter_by(key=institution.parent_key).one() if institution.parent_key else None
-    sqlinstitution.registry = db_session.query(SQLRegistry).filter_by(key=institution.registry_key).one()
-    db_session.add(sqlinstitution)
-    db_session.commit()
+    try:
+        sqlinstitution.parent = session.query(SQLInstitution).filter_by(key=institution.parent_key).one() if institution.parent_key else None
+        sqlinstitution.registry = session.query(SQLRegistry).filter_by(key=institution.registry_key).one()
+    except NoResultFound:
+        raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, "Referenced entity not found.")
+
+    session.add(sqlinstitution)
+    session.commit()
+
     return institution
