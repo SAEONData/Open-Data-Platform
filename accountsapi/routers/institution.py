e1b042477d00fdb5c270bc3eeeebc87c35d6e57a
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-02-13 14:44:43 +0200

    Make the institution creation API create-only
    Rather than create-or-update, which introduces the possibility of things being mucked up by badly behaved clients

--- accountsapi/routers/institution.py
+++ accountsapi/routers/institution.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import IntegrityError
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
 from odpaccounts.models.institution import Institution as InstitutionORM
@@ -15,22 +16,26 @@ router = APIRouter()
 
 
 @router.post('/', response_model=Institution)
-async def create_or_update_institution(
+async def create_institution(
         institution: Institution,
         session: Session = Depends(db_session),
 ):
-    institution_orm = session.query(InstitutionORM).filter_by(key=institution.key).one_or_none()
-    if not institution_orm:
-        institution_orm = InstitutionORM(key=institution.key)
-
-    institution_orm.name = institution.name
+    institution_orm = InstitutionORM(
+        key=institution.key,
+        name=institution.name,
+    )
     try:
         institution_orm.parent = session.query(InstitutionORM).filter_by(key=institution.parent_key).one() if institution.parent_key else None
     except NoResultFound:
         raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, "Parent institution not found.")
 
     session.add(institution_orm)
-    session.commit()
+    try:
+        session.commit()
+    except IntegrityError as e:
+        # catch unique key violations
+        session.rollback()
+        raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, str(e))
 
     return institution
 
