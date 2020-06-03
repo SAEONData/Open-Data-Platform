c5df67a63b60a5d398031843d61f530ba202aa50
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-06-03 15:10:34 +0200

    Capture annotations required by the initial "captured" workflow state

--- odpapi/models/metadata.py
+++ odpapi/models/metadata.py
@@ -1,10 +1,16 @@
 from typing import Dict, Optional
+from enum import Enum
 
-from pydantic import BaseModel, Field, UUID4
+from pydantic import BaseModel, Field, UUID4, AnyHttpUrl
 
 # adapted from https://www.crossref.org/blog/dois-and-matching-regular-expressions
 # Note: this allows either a DOI string or an empty string
 DOI_REGEX = r'^(|10\.\d{4,}(\.\d+)*/[-._;()/:a-zA-Z0-9]+)$'
+
+
+class CaptureMethod(str, Enum):
+    wizard = 'wizard'
+    harvester = 'harvester'
 
 
 class MetadataRecordIn(BaseModel):
@@ -13,6 +19,10 @@ class MetadataRecordIn(BaseModel):
     metadata: Dict
     doi: str = Field('', regex=DOI_REGEX)
     auto_assign_doi: bool = False
+    terms_conditions_accepted: bool
+    data_agreement_accepted: bool
+    data_agreement_url: AnyHttpUrl
+    capture_method: CaptureMethod
 
 
 class MetadataRecord(BaseModel):
