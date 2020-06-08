3d67917cf55db59c1ab59117eba7e7b7e8146a91
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-06-08 15:46:40 +0200

    Specify the final landing page after login/logout completion

--- odpidentity/views/hydra_oauth2.py
+++ odpidentity/views/hydra_oauth2.py
@@ -4,4 +4,4 @@ from odpaccounts.models.oauth2_token import OAuth2Token
 from hydra_oauth2 import HydraOAuth2Blueprint
 
 # Note: the blueprint name 'odpidentity' becomes the provider in the token model
-bp = HydraOAuth2Blueprint('odpidentity', __name__, db_session, User, OAuth2Token)
+bp = HydraOAuth2Blueprint('odpidentity', __name__, db_session, User, OAuth2Token, 'home.index')
