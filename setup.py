f9f9006e2cf964b8d40ec2ff934cba2d60f0f552
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-07-02 11:55:35 +0200

    Use the generic namespace package find function

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 version = '1.1.0'
 
@@ -10,7 +10,7 @@ setup(
     author='Mark Jacobson',
     author_email='mark@saeon.ac.za',
     license='MIT',
-    packages=find_packages() + ['odp_api_adapters'],
+    packages=find_namespace_packages(),
     python_requires='~=3.8',
     install_requires=[
         'elasticsearch>=6.0.0,<7.0.0',
