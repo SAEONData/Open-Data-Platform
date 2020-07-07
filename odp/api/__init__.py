244068bb8bf313ff97e377506a2beda1d6d57959
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-07-07 18:42:54 +0200

    Remove redundant imports
    These imports are now done in the DB models' __init__.py

--- odp/api/__init__.py
+++ odp/api/__init__.py
@@ -4,13 +4,6 @@ def db_session():
     :return: SQLAlchemy session
     """
     from odp.db import session
-    from odp.db.models.user import User
-    from odp.db.models.member import Member
-    from odp.db.models.privilege import Privilege
-    from odp.db.models.role import Role
-    from odp.db.models.scope import Scope
-    from odp.db.models.capability import Capability
-    from odp.db.models.institution import Institution
     try:
         yield session
     finally:
