e326addc16c8f82186449881bcfa75e05ea2403e
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-08-04 16:10:48 +0200

    Add constraint to institution key field
    To ensure that institutions play nicely with CKAN.

--- odp/api/models/institution.py
+++ odp/api/models/institution.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from odp.api.models import KEY_REGEX
 
 
 class Institution(BaseModel):
-    key: str
+    key: str = Field(..., regex=KEY_REGEX)
     name: str
     parent_key: Optional[str]
