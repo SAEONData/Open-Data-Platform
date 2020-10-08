63a929637adbb24972bc472ed254f1e36e9242b0
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2020-10-08 17:41:28 +0200

    Add creation timestamp and index to metadata_status DB model
    This will enable us to provide a stable ordering of records, suitable for pagination, from the catalogue harvest endpoint.

--- odp/db/models/metadata_status.py
+++ odp/db/models/metadata_status.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, String, Boolean, TIMESTAMP
+from datetime import datetime, timezone
+
+from sqlalchemy import Column, String, Boolean, TIMESTAMP, Index
 from sqlalchemy.dialects.postgresql import JSONB
 
 from odp.db import Base
@@ -12,5 +14,11 @@ class MetadataStatus(Base):
     metadata_id = Column(String, primary_key=True)
     catalogue_record = Column(JSONB, nullable=False)
     published = Column(Boolean, nullable=False)
+    created = Column(TIMESTAMP(timezone=True), nullable=False, default=datetime.now(timezone.utc))
     updated = Column(TIMESTAMP(timezone=True), nullable=False)
     checked = Column(TIMESTAMP(timezone=True), nullable=False)
+
+    # We want to be able to sort on created to provide a stable ordering at the
+    # catalogue harvest endpoint. However, given that a series of consecutive
+    # records might have identical timestamps, we order by id as well.
+    __table_args__ = (Index('ix_metadata_status_created_metadata_id', 'created', 'metadata_id'),)
