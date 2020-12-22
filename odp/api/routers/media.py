6589c1a84da0f6cc8ae42e04c90bb301042e918a
bryan@saeon.ac.za <bryan@saeon.ac.za>
2020-12-22 15:06:23 +0200

    Extended ODP API with /media/{file_path} endpoint. It obtains the download link for the corresponding file_path from the NextCloud based media repository.

---
+++ odp/api/routers/media.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
+from fastapi.responses import RedirectResponse
+
+from odp.lib.exceptions import MediaRepositoryError
+from odp.api.dependencies.media import get_media_repo_client
+
+router = APIRouter()
+
+@router.get('/{file_path:path}')
+async def get_media_download(
+        file_path: str,
+        media_repo_client: str = Depends(get_media_repo_client),
+):
+    try:
+        download_link = media_repo_client.get_download_link(file_path)
+        return RedirectResponse(download_link)
+    except MediaRepositoryError as e:
+        raise HTTPException(e.status_code, e.error_detail) from e
