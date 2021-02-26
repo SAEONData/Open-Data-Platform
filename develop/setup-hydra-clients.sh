0187cfecf7be07c28edd1e511efc7c4ce786aaa4
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-02-26 10:52:22 +0200

    Add an OAuth2 client for local API testing
    And access the local Hydra instance on plain http.

--- develop/setup-hydra-clients.sh
+++ develop/setup-hydra-clients.sh
@@ -4,23 +4,33 @@ echo "Loading environment variables..."
 source .env
 
 echo "Deleting existing clients..."
-docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=https://hydra:4445 ${HYDRA_IMAGE} \
+docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=http://hydra:4445 ${HYDRA_IMAGE} \
   clients delete --skip-tls-verify odp-init
-docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=https://hydra:4445 ${HYDRA_IMAGE} \
+docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=http://hydra:4445 ${HYDRA_IMAGE} \
+  clients delete --skip-tls-verify odp-client
+docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=http://hydra:4445 ${HYDRA_IMAGE} \
   clients delete --skip-tls-verify ${ODP_ADMIN_UI_CLIENT_ID}
-docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=https://hydra:4445 ${HYDRA_IMAGE} \
+docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=http://hydra:4445 ${HYDRA_IMAGE} \
   clients delete --skip-tls-verify ${CKAN_CLIENT_ID}
 
 echo "Creating OAuth2 client for local ODP init script..."
-docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=https://hydra:4445 ${HYDRA_IMAGE} \
+docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=http://hydra:4445 ${HYDRA_IMAGE} \
   clients create --skip-tls-verify \
     --id odp-init \
     --secret secret \
     --grant-types client_credentials \
     --scope ODP.Metadata,ODP.Admin
 
+echo "Creating OAuth2 client for local API testing..."
+docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=http://hydra:4445 ${HYDRA_IMAGE} \
+  clients create --skip-tls-verify \
+    --id odp-client \
+    --secret secret \
+    --grant-types client_credentials \
+    --scope ODP.Metadata,ODP.Admin,ODP.Catalogue
+
 echo "Creating OAuth2 client for local admin service..."
-docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=https://hydra:4445 ${HYDRA_IMAGE} \
+docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=http://hydra:4445 ${HYDRA_IMAGE} \
   clients create --skip-tls-verify \
     --id ${ODP_ADMIN_UI_CLIENT_ID} \
     --secret ${ODP_ADMIN_UI_CLIENT_SECRET} \
@@ -31,7 +41,7 @@ docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=https://hydra:4445 ${HY
     --post-logout-callbacks ${ODP_ADMIN_UI_URL}/oauth2/logged_out
 
 echo "Creating OAuth2 client for local metadata manager..."
-docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=https://hydra:4445 ${HYDRA_IMAGE} \
+docker run -it --rm --network odp-net -e HYDRA_ADMIN_URL=http://hydra:4445 ${HYDRA_IMAGE} \
   clients create --skip-tls-verify \
     --id ${CKAN_CLIENT_ID} \
     --secret ${CKAN_CLIENT_SECRET} \
