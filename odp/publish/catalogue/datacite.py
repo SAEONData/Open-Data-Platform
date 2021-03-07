f741a46033a27293ec6af369ed70efa58e11dc6e
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-03-07 18:45:06 +0200

    Implement a few improvements to DataCite publishing

--- odp/publish/catalogue/datacite.py
+++ odp/publish/catalogue/datacite.py
@@ -39,9 +39,10 @@ class DataciteCatalogue(Catalogue):
     def synchronize(self) -> None:
         published = 0
         unpublished = 0
+        unchanged = 0
         errors = 0
         try:
-            updated_records = session.query(CatalogueRecord, DataciteRecord). \
+            check_records = session.query(CatalogueRecord, DataciteRecord). \
                 outerjoin(DataciteRecord). \
                 filter(not_(  # exclude records without DOIs
                     CatalogueRecord.catalogue_record.comparator.contains({'doi': None})
@@ -49,14 +50,19 @@ class DataciteCatalogue(Catalogue):
                 filter(or_(
                     DataciteRecord.metadata_id == None,
                     DataciteRecord.checked < CatalogueRecord.updated,
+                    # check if the DOI resolution URL that we publish to DataCite must
+                    # be updated; unlike a change in metadata, this might result from
+                    # a config (or code) change, but not from a catalogue record update
+                    DataciteRecord.url != self.doi_landing_page_base_url + '/' + DataciteRecord.doi,
                 )). \
                 limit(self.batch_size).all()
 
-            # clear errors and retries for updated records
-            for catrec, dcrec in updated_records:
+            # clear errors and retries for records that we have selected to check
+            for catrec, dcrec in check_records:
                 if dcrec is not None and dcrec.error is not None:
                     dcrec.error = None
                     dcrec.retries = None
+                    dcrec.save()
 
             failed_ids = session.query(CatalogueRecord.metadata_id).join(DataciteRecord).filter(
                 and_(
@@ -65,28 +71,30 @@ class DataciteCatalogue(Catalogue):
                 ),
             ).limit(self.batch_size).all()
 
-            syncable_ids = [catrec.metadata_id for catrec, dcrec in updated_records] + \
+            syncable_ids = [catrec.metadata_id for catrec, dcrec in check_records] + \
                            [record_id for record_id, in failed_ids]
-            logger.info(f"Selected {len(updated_records)} updated records and {len(failed_ids)} "
-                        "previously failed records to sync with DataCite")
+            logger.info(f"Selected {len(syncable_ids)} records to synchronize with DataCite")
             for record_id in syncable_ids:
-                pub, unpub, err = self._syncrecord(record_id)
+                pub, unpub, unchg, err = self._syncrecord(record_id)
                 published += pub
                 unpublished += unpub
+                unchanged += unchg
                 errors += err
 
         except Exception as e:
             logger.critical(str(e))
         finally:
             session.remove()
-            logger.info(f"Published {published} records to DataCite; "
-                        f"un-published {unpublished} records from DataCite; "
+            logger.info(f"{published} published; "
+                        f"{unpublished} un-published; "
+                        f"{unchanged} unchanged; "
                         f"{errors} errors")
 
-    def _syncrecord(self, record_id: str) -> Tuple[bool, bool, bool]:
+    def _syncrecord(self, record_id: str) -> Tuple[bool, bool, bool, bool]:
         logger.debug(f"Syncing record {record_id}")
         published = False
         unpublished = False
+        unchanged = False
         error = False
 
         with transaction():
@@ -95,60 +103,65 @@ class DataciteCatalogue(Catalogue):
                 one()
 
             doi = catrec.catalogue_record['doi']
-            if dcrec is None and doi is not None:
+            if dcrec is None:
                 dcrec = DataciteRecord(metadata_id=record_id, doi=doi, published=False)
 
-            if dcrec is not None:
-                try:
-                    datacite_record = DataciteRecordIn(
-                        doi=doi,
-                        url=f'{self.doi_landing_page_base_url}/{doi}',
-                        metadata=catrec.catalogue_record['metadata'],
+            try:
+                datacite_record = DataciteRecordIn(
+                    doi=doi,
+                    url=f'{self.doi_landing_page_base_url}/{doi}',
+                    metadata=catrec.catalogue_record['metadata'],
+                )
+                datacite_record_dict = datacite_record.dict()
+            except pydantic.ValidationError:
+                datacite_record = None
+                datacite_record_dict = None
+
+            publish = catrec.published and doi is not None and datacite_record is not None
+            try:
+                if dcrec.published and (not publish or dcrec.doi.lower() != doi.lower()):
+                    # the record is currently published and should be unpublished;
+                    # if the DOI has changed, we must also first unpublish the record
+                    logger.info(f"Unpublishing record {record_id} with DOI {dcrec.doi}")
+                    self.datacite.unpublish_doi(
+                        dcrec.doi,
                     )
-                    datacite_record_dict = datacite_record.dict()
-                except pydantic.ValidationError:
-                    datacite_record = None
-                    datacite_record_dict = None
+                    dcrec.published = False
+                    dcrec.updated = datetime.now(timezone.utc)
+                    unpublished = True
 
-                publish = catrec.published and doi is not None and datacite_record is not None
-                try:
-                    if dcrec.published and (not publish or dcrec.doi.lower() != doi.lower()):
-                        # the record is currently published and should be unpublished;
-                        # if the DOI has changed, we must also first unpublish the record
-                        logger.info(f"Unpublishing record {record_id} with DOI {dcrec.doi}")
-                        self.datacite.unpublish_doi(
-                            dcrec.doi,
-                        )
-                        dcrec.published = False
-                        dcrec.updated = datetime.now(timezone.utc)
-                        unpublished = True
+                if publish and (not dcrec.published or datacite_record_dict != {
+                    'doi': dcrec.doi,
+                    'url': dcrec.url,
+                    'metadata': dcrec.metadata_,
+                }):
+                    # the record should be published; it is either not currently published,
+                    # or it is published but one or more properties has changed
+                    logger.info(f"Publishing record {record_id} with DOI {doi}")
+                    self.datacite.publish_doi(
+                        datacite_record,
+                    )
+                    dcrec.doi = doi
+                    dcrec.url = datacite_record.url
+                    dcrec.metadata_ = datacite_record.metadata
+                    dcrec.published = True
+                    dcrec.updated = datetime.now(timezone.utc)
+                    published = True
 
-                    if publish and (not dcrec.published or dcrec.datacite_record != datacite_record_dict):
-                        # the record should be published; it is either not currently published,
-                        # or it is published but one or more properties has changed
-                        logger.info(f"Publishing record {record_id} with DOI {doi}")
-                        self.datacite.publish_doi(
-                            datacite_record,
-                        )
-                        dcrec.doi = doi
-                        dcrec.datacite_record = datacite_record_dict
-                        dcrec.published = True
-                        dcrec.updated = datetime.now(timezone.utc)
-                        published = True
+                if not (published or unpublished):
+                    logger.debug(f"No change for record {record_id}")
+                    unchanged = True
 
-                    if not (published or unpublished):
-                        logger.debug(f"No change for record {record_id}")
+                dcrec.error = None
+                dcrec.retries = None
 
-                    dcrec.error = None
-                    dcrec.retries = None
+            except DataciteError as e:
+                dcrec.error = f'{e.status_code}: {e.error_detail}'
+                dcrec.retries = dcrec.retries + 1 if dcrec.retries is not None else 0
+                logger.error(f"Error syncing record {record_id} with DataCite: {dcrec.error}")
+                error = True
 
-                except DataciteError as e:
-                    dcrec.error = f'{e.status_code}: {e.error_detail}'
-                    dcrec.retries = dcrec.retries + 1 if dcrec.retries is not None else 0
-                    logger.error(f"Error syncing record {record_id} with DataCite: {dcrec.error}")
-                    error = True
+            dcrec.checked = datetime.now(timezone.utc)
+            dcrec.save()
 
-                dcrec.checked = datetime.now(timezone.utc)
-                dcrec.save()
-
-        return published, unpublished, error
+        return published, unpublished, unchanged, error
