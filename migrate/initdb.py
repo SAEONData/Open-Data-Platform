095fe88705a6801c2c96c0f5a356b075d53bac19
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-08-27 12:54:23 +0200

    Fix DB init script

--- migrate/initdb.py
+++ migrate/initdb.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+import pathlib
+import sys
+
+rootdir = pathlib.Path(__file__).parent.parent
+sys.path.append(str(rootdir))
+
 """Initialize the ODP database schema."""
 
 if __name__ == "__main__":
