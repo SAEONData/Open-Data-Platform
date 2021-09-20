70c2a1546d2706d54b1406798ec7a337510186db
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-09-20 14:09:14 +0200

    Commit or rollback the transaction at the end of a request
    Based on the HTTP status code of the response.

--- odp/admin/db.py
+++ odp/admin/db.py
@@ -1,7 +1,17 @@
 def init_app(app):
-    from odp.db import session
+    from odp.db import Session
+
+    @app.after_request
+    def commit_transaction(response):
+        """Commit any open transaction if the request was successful."""
+        if 200 <= response.status_code < 300:
+            Session.commit()
+        else:
+            Session.rollback()
+
+        return response
 
     @app.teardown_request
     def release_db_resources(exc):
         """Release DB transaction/connection resources at the end of a request."""
-        session.remove()
+        Session.remove()
