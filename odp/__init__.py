5a4fe2464cb612c29df1c936453c9078dd457d66
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-10-06 09:48:20 +0200

    Create a usable admin user

--- odp/__init__.py
+++ odp/__init__.py
@@ -1,7 +1,4 @@
-import uuid
 from enum import Enum
-
-from sqlalchemy import text
 
 import odp.config
 import odp.db
@@ -30,64 +27,3 @@ class ODPScope(str, Enum):
     SCOPE_VIEW = 'odp.scope:view'
     USER_MANAGE = 'odp.user:manage'
     USER_VIEW = 'odp.user:view'
-
-
-def create_odp_schema():
-    """Create the ODP database schema.
-
-    Only tables that do not exist are created. To modify existing table
-    definitions, use Alembic migrations.
-    """
-    import odp.db.models
-    odp.db.Base.metadata.create_all(odp.db.engine)
-
-
-def create_odp_scopes():
-    """Create any ODP system scopes that do not exist."""
-    with odp.db.engine.begin() as conn:
-        scopes = ','.join(f"('{s.value}')" for s in ODPScope)
-        conn.execute(text(f"INSERT INTO scope (id) VALUES {scopes} "
-                          "ON CONFLICT DO NOTHING"))
-
-
-def create_odp_admin_role():
-    """Create the ODP admin role if it does not exist."""
-    with odp.db.engine.begin() as conn:
-        conn.execute(text("INSERT INTO role (id, name) VALUES "
-                          f"('{ODP_ADMIN_ROLE}', 'ODP Admin') "
-                          "ON CONFLICT DO NOTHING"))
-
-        role_scopes = ','.join(f"('{ODP_ADMIN_ROLE}', '{s.value}')" for s in ODPScope)
-        conn.execute(text(f"INSERT INTO role_scope (role_id, scope_id) VALUES {role_scopes} "
-                          "ON CONFLICT DO NOTHING"))
-
-
-def create_odp_admin_user():
-    """Ensure that there is an admin user."""
-    with odp.db.engine.begin() as conn:
-        result = conn.execute(text(f"SELECT 1 FROM user_role WHERE role_id = '{ODP_ADMIN_ROLE}'"))
-
-        if result.first() is None:
-            user_id = str(uuid.uuid4())
-            email = input("Enter the admin user's email address: ")
-
-            conn.execute(text('INSERT INTO "user" (id, email, active, verified) VALUES '
-                              f"('{user_id}', '{email}', true, true) "
-                              "ON CONFLICT DO NOTHING"))
-
-            conn.execute(text('INSERT INTO user_role (user_id, role_id) '
-                              f"SELECT u.id, '{ODP_ADMIN_ROLE}' FROM \"user\" u "
-                              f"WHERE u.email = '{email}'"))
-
-
-def create_odp_app_client():
-    """Create client config for the ODP app."""
-    with odp.db.engine.begin() as conn:
-        client_id = odp.config.config.ODP.APP.CLIENT_ID
-        conn.execute(text("INSERT INTO client (id, name) VALUES "
-                          f"('{client_id}', '(: The Open Data Platform App :)') "
-                          "ON CONFLICT DO NOTHING"))
-
-        client_scopes = ','.join(f"('{client_id}', '{s.value}')" for s in ODPScope)
-        conn.execute(text(f"INSERT INTO client_scope (client_id, scope_id) VALUES {client_scopes} "
-                          "ON CONFLICT DO NOTHING"))
