9baeadd8ad825f9e8fa6cbd75f0b7b9a1675d2d5
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-11-03 09:06:06 +0200

    Add auth to providers UI

--- odp/ui/views/providers.py
+++ odp/ui/views/providers.py
@@ -1,24 +1,29 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 
+from odp import ODPScope
 from odp.ui import api
+from odp.ui.auth import authorize
 from odp.ui.forms import ProviderForm
 
 bp = Blueprint('providers', __name__)
 
 
 @bp.route('/')
+@authorize(ODPScope.PROVIDER_READ)
 def index():
     providers = api.get('/provider/')
     return render_template('provider_list.html', providers=providers)
 
 
 @bp.route('/<id>')
+@authorize(ODPScope.PROVIDER_READ)
 def view(id):
     provider = api.get(f'/provider/{id}')
     return render_template('provider_view.html', provider=provider)
 
 
 @bp.route('/new', methods=('GET', 'POST'))
+@authorize(ODPScope.PROVIDER_ADMIN)
 def create():
     form = ProviderForm(request.form)
 
@@ -34,6 +39,7 @@ def create():
 
 
 @bp.route('/<id>/edit', methods=('GET', 'POST'))
+@authorize(ODPScope.PROVIDER_ADMIN)
 def edit(id):
     provider = api.get(f'/provider/{id}')
     form = ProviderForm(request.form, data=provider)
@@ -50,6 +56,7 @@ def edit(id):
 
 
 @bp.route('/<id>/delete', methods=('POST',))
+@authorize(ODPScope.PROVIDER_ADMIN)
 def delete(id):
     api.delete(f'/provider/{id}')
     flash(f'Provider {id} has been deleted.', category='success')
