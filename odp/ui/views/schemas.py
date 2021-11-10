6249d5d2a64e51e03a1687fe6058f84d8f76e76e
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-11-10 11:06:21 +0200

    Add schema UI

--- odp/ui/views/schemas.py
+++ odp/ui/views/schemas.py
@@ -1,8 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
+
+from odp import ODPScope
+from odp.ui import api
+from odp.ui.auth import authorize
 
 bp = Blueprint('schemas', __name__)
 
 
 @bp.route('/')
+@authorize(ODPScope.SCHEMA_READ)
+@api.wrapper
 def index():
-    return render_template('schema_lists.html')
+    schemas = api.get('/schema/')
+    return render_template('schema_list.html', schemas=schemas)
+
+
+@bp.route('/<id>')
+@authorize(ODPScope.SCHEMA_READ)
+@api.wrapper
+def view(id):
+    schema = api.get(f'/schema/{id}')
+    return render_template('schema_view.html', schema=schema)
+
+
+@bp.route('/new')
+def create():
+    abort(404)
+
+
+@bp.route('/<id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    abort(404)
+
+
+@bp.route('/<id>/delete', methods=('POST',))
+def delete(id):
+    abort(404)
