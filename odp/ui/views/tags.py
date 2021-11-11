3b54abd62f7a8e0d271a5037df1a9cd332c05eda
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2021-11-11 12:33:21 +0200

    Implement tags UI

--- odp/ui/views/tags.py
+++ odp/ui/views/tags.py
@@ -1,8 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
+
+from odp import ODPScope
+from odp.ui import api
+from odp.ui.auth import authorize
 
 bp = Blueprint('tags', __name__)
 
 
 @bp.route('/')
+@authorize(ODPScope.TAG_READ)
+@api.wrapper
 def index():
-    return render_template('tag_lists.html')
+    tags = api.get('/tag/')
+    return render_template('tag_list.html', tags=tags)
+
+
+@bp.route('/<id>')
+@authorize(ODPScope.TAG_READ)
+@api.wrapper
+def view(id):
+    tag = api.get(f'/tag/{id}')
+    return render_template('tag_view.html', tag=tag)
+
+
+@bp.route('/new')
+def create():
+    abort(404)
+
+
+@bp.route('/<id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    abort(404)
+
+
+@bp.route('/<id>/delete', methods=('POST',))
+def delete(id):
+    abort(404)
