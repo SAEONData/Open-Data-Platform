26bb8fb72d67d4b0f0a474c1f66becef5e3865ac
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2022-02-18 16:29:26 +0200

    Delete old code

--- odp/api/lib/paging.py
+++ odp/api/lib/paging.py
@@ -1,39 +1,16 @@
-from dataclasses import dataclass
 from math import ceil
-from typing import Generic, TypeVar, List, Callable
+from typing import Callable, Generic, List, TypeVar
 
 from fastapi import HTTPException, Query
 from pydantic import BaseModel
 from pydantic.generics import GenericModel
-from sqlalchemy import select, func
+from sqlalchemy import func, select
 from sqlalchemy.engine import Row
 from sqlalchemy.exc import CompileError
 from sqlalchemy.sql import Select
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-from odp.db import Session, Base
-
-
-@dataclass
-class Pager:
-    sort: str
-    skip: int
-    limit: int
-
-
-class Paging:
-    def __init__(self, sort_enum):
-        self.sort_keys = [e.value for e in sort_enum]
-
-    async def __call__(self, sort: str = None, skip: int = 0, limit: int = 100) -> Pager:
-        if sort and sort not in self.sort_keys:
-            raise HTTPException(
-                HTTP_422_UNPROCESSABLE_ENTITY,
-                f'Sort key must be one of {self.sort_keys}',
-            )
-        sort = sort or self.sort_keys[0]
-        return Pager(sort=sort, skip=skip, limit=limit)
-
+from odp.db import Base, Session
 
 ModelT = TypeVar('ModelT', bound=BaseModel)
 
