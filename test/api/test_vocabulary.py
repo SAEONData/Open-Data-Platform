919c0cd8530b68a79a838fb1f1f5f4e8d427ccfa
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2022-07-25 16:06:06 +0200

    Add vocabulary API tests
    With a few stubs for now.

---
+++ test/api/test_vocabulary.py
@@ -0,0 +1,204 @@
+from random import randint
+
+import pytest
+from sqlalchemy import select
+
+from odp import ODPScope
+from odp.db import Session
+from odp.db.models import SchemaType, Scope, ScopeType, Vocabulary, VocabularyTerm
+from test.api import all_scopes, all_scopes_excluding, assert_empty_result, assert_forbidden, assert_not_found
+from test.factories import SchemaFactory, VocabularyFactory, VocabularyTermFactory
+
+
+@pytest.fixture
+def vocabulary_batch():
+    """Create and commit a batch of Vocabulary instances,
+    with associated terms."""
+    return [VocabularyFactory() for _ in range(randint(3, 5))]
+
+
+@pytest.fixture
+def vocabulary_batch_no_terms():
+    """Create and commit a batch of Vocabulary instances,
+    without any associated terms."""
+    return [VocabularyFactory(terms=[]) for _ in range(randint(3, 5))]
+
+
+def term_ids(vocabulary):
+    return tuple(sorted(term.term_id for term in vocabulary.terms))
+
+
+def assert_db_state(vocabularies):
+    """Verify that the DB vocabulary table contains the given vocabulary batch."""
+    Session.expire_all()
+    result = Session.execute(select(Vocabulary)).scalars().all()
+    result.sort(key=lambda v: v.id)
+    vocabularies.sort(key=lambda v: v.id)
+    assert len(result) == len(vocabularies)
+    for n, row in enumerate(result):
+        assert row.id == vocabularies[n].id
+        assert row.scope_id == vocabularies[n].scope_id
+        assert row.scope_type == 'odp'
+        assert row.schema_id == vocabularies[n].schema_id
+        assert row.schema_type == 'vocabulary'
+        dbterms = Session.execute(select(VocabularyTerm).where(VocabularyTerm.vocabulary_id == row.id)).scalars().all()
+        dbterms.sort(key=lambda t: t.term_id)
+        terms = sorted(vocabularies[n].terms, key=lambda t: t.term_id)
+        assert len(dbterms) == len(terms)
+        for m, trow in enumerate(dbterms):
+            assert trow.vocabulary_id == terms[m].vocabulary_id == row.id
+            assert trow.term_id == terms[m].term_id
+            assert trow.data == terms[m].data
+
+
+def assert_json_result(response, json, vocabulary):
+    """Verify that the API result matches the given vocabulary object."""
+    assert response.status_code == 200
+    assert json['id'] == vocabulary.id
+    assert json['scope_id'] == vocabulary.scope_id
+    assert json['schema_id'] == vocabulary.schema_id
+    assert json['schema_uri'] == vocabulary.schema.uri
+    assert json['schema_']['$id'] == vocabulary.schema.uri
+
+    json_terms = sorted(json['terms'], key=lambda t: t['id'])
+    vocab_terms = sorted(vocabulary.terms, key=lambda t: t.term_id)
+    assert [(json_term['id'], json_term['data']) for json_term in json_terms] == \
+           [(vocab_term.term_id, vocab_term.data) for vocab_term in vocab_terms]
+
+
+def assert_json_results(response, json, vocabularies):
+    """Verify that the API result list matches the given vocabulary batch."""
+    items = json['items']
+    assert json['total'] == len(items) == len(vocabularies)
+    items.sort(key=lambda i: i['id'])
+    vocabularies.sort(key=lambda v: v.id)
+    for n, vocabulary in enumerate(vocabularies):
+        assert_json_result(response, items[n], vocabulary)
+
+
+@pytest.mark.parametrize('scopes', [
+    [ODPScope.VOCABULARY_READ],
+    [],
+    all_scopes,
+    all_scopes_excluding(ODPScope.VOCABULARY_READ),
+])
+def test_list_vocabularies(api, vocabulary_batch, scopes):
+    authorized = ODPScope.VOCABULARY_READ in scopes
+    r = api(scopes).get('/vocabulary/')
+    if authorized:
+        assert_json_results(r, r.json(), vocabulary_batch)
+    else:
+        assert_forbidden(r)
+    assert_db_state(vocabulary_batch)
+
+
+@pytest.mark.parametrize('scopes', [
+    [ODPScope.VOCABULARY_READ],
+    [],
+    all_scopes,
+    all_scopes_excluding(ODPScope.VOCABULARY_READ),
+])
+def test_get_vocabulary(api, vocabulary_batch, scopes):
+    authorized = ODPScope.VOCABULARY_READ in scopes
+    r = api(scopes).get(f'/vocabulary/{vocabulary_batch[2].id}')
+    if authorized:
+        assert_json_result(r, r.json(), vocabulary_batch[2])
+    else:
+        assert_forbidden(r)
+    assert_db_state(vocabulary_batch)
+
+
+def test_get_vocabulary_not_found(api, vocabulary_batch):
+    scopes = [ODPScope.VOCABULARY_READ]
+    r = api(scopes).get('/vocabulary/foo')
+    assert_not_found(r)
+    assert_db_state(vocabulary_batch)
+
+
+def prepare_project_vocabulary(vocab):
+    vocab.scope = Session.get(Scope, (ODPScope.VOCABULARY_PROJECT, ScopeType.odp)) or \
+                  Scope(id=ODPScope.VOCABULARY_PROJECT, type=ScopeType.odp)
+    vocab.schema = SchemaFactory(
+        type=SchemaType.vocabulary,
+        uri='https://odp.saeon.ac.za/schema/vocabulary/project',
+    )
+    vocab.save()
+    Session.commit()
+    return vocab
+
+
+@pytest.mark.parametrize('scopes', [
+    [ODPScope.VOCABULARY_PROJECT],
+    [],
+    all_scopes,
+    all_scopes_excluding(ODPScope.VOCABULARY_PROJECT),
+])
+def test_create_term(api, vocabulary_batch, scopes):
+    authorized = ODPScope.VOCABULARY_PROJECT in scopes
+    client = api(scopes)
+
+    modified_vocab_batch = vocabulary_batch.copy()
+    modified_vocab = prepare_project_vocabulary(modified_vocab_batch[2])
+    modified_vocab.terms += [term := VocabularyTermFactory.build(
+        vocabulary=modified_vocab,
+        data={'title': 'Some Project'},
+    )]
+
+    r = client.post(f'/vocabulary/{modified_vocab.id}/', json=dict(
+        id=term.term_id,
+        data=term.data,
+    ))
+
+    if authorized:
+        assert_empty_result(r)
+        assert_db_state(modified_vocab_batch)
+    else:
+        assert_forbidden(r)
+        assert_db_state(vocabulary_batch)
+
+
+def test_create_term_conflict():
+    pass  # todo
+
+
+def test_create_term_invalid():
+    pass  # todo
+
+
+@pytest.mark.parametrize('scopes', [
+    [ODPScope.VOCABULARY_PROJECT],
+    [],
+    all_scopes,
+    all_scopes_excluding(ODPScope.VOCABULARY_PROJECT),
+])
+def test_update_term(api, vocabulary_batch, scopes):
+    authorized = ODPScope.VOCABULARY_PROJECT in scopes
+    client = api(scopes)
+
+    modified_vocab_batch = vocabulary_batch.copy()
+    modified_vocab = prepare_project_vocabulary(modified_vocab_batch[2])
+    modified_vocab.terms[2] = (term := VocabularyTermFactory.build(
+        vocabulary=modified_vocab,
+        term_id=modified_vocab.terms[2].term_id,
+        data={'title': 'Some Project'},
+    ))
+
+    r = client.put(f'/vocabulary/{modified_vocab.id}/', json=dict(
+        id=term.term_id,
+        data=term.data,
+    ))
+
+    if authorized:
+        assert_empty_result(r)
+        assert_db_state(modified_vocab_batch)
+    else:
+        assert_forbidden(r)
+        assert_db_state(vocabulary_batch)
+
+
+def test_update_term_not_found():
+    pass  # todo
+
+
+def test_update_term_invalid():
+    pass  # todo
